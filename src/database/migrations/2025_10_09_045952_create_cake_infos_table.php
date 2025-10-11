727bd2709a9b21ab6559707fe5444b4f2afe8d13
v420v <ibuki420v@gmail.com>
2025-10-11 16:30:16 +0900

    add: マイグレーションファイルの追加

---
+++ src/database/migrations/2025_10_09_045952_create_cake_infos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cake_infos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained('products');
+            $table->string('catchphrase');
+            $table->decimal('calorie', 6, 2);
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cake_infos');
+    }
+};
