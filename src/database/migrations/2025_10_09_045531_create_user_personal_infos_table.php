16155605d5921a761cc1814805c69dc1f5c168e4
v420v <ibuki420v@gmail.com>
2025-10-11 16:51:13 +0900

    fix: user_id 漏れを修正

--- src/database/migrations/2025_10_09_045531_create_user_personal_infos_table.php
+++ src/database/migrations/2025_10_09_045531_create_user_personal_infos_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('user_personal_infos', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users');
             $table->string('first_name');
             $table->string('last_name');
             $table->date('birthdate');
